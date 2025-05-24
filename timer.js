55f3739b7d875f952fc695cd025fd8eeeb77508f
shaileshchaudhary95 <chaudharyshailesh950@gmail.com>
2025-05-24 17:45:44 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,32 @@
+/*
+Task 4:
+create timer method which is return array of seconds at the last second
+input : 5
+result : 
+ time 1 second
+ time 2 second
+ time 3 second
+ time 4 second
+ result is [1,2,3,4]
+
+*/
+
+function createTimer(seconds) {
+    const result = [];
+
+    for (let i = 1; i < seconds; i++) {
+        setTimeout(() => {
+
+            console.log(`time ${i} second`);
+
+            result.push(i);
+
+            if (i === seconds - 1) {
+                console.log('result is', result);
+            }
+
+        }, i * 1000);
+    }
+}
+
+createTimer(5)
