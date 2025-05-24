573d93b35c85c5c798f78cdd76085a6797cc4fdd
shaileshchaudhary95 <chaudharyshailesh950@gmail.com>
2025-05-24 07:38:24 +0530

    Pair.js

--- pair.js
+++ pair.js
@@ -1,3 +1,16 @@
+/*
+Please Complete Below Task  ASAP
+
+Input: arr[] = [0, -1, 2, -3, 1], target = -2
+Output: true
+Explanation: There is a pair (1, -3) with the sum equal to given target, 1 + (-3) = -2.
+
+Input: arr[] = [1, -2, 1, 0, 5], target = 0
+Output: false
+Explanation: There is no pair with sum equals to given target.
+*/
+
+
 function myPair(arr, target) {
     const result = {};
 
@@ -18,7 +31,7 @@ console.log(myPair([1, -2, 1, 0, 5], 0));
 
 
 
-// +++++++++++++++++++++ solution working idea down +++++++++++++++++++++++++++
+// +++++++++++++++++++++ solution working idea +++++++++++++++++++++++++++
 
 /*
 
